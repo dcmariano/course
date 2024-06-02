3dbd10604c90311a56a22e5c8083602d276401df
Davi Cristian Mariano <davicristianmariano7@gmail.com>
2024-06-02 14:18:01 -0300

    Chess

---
+++ chess-system/src/chess/ChessPiece.java
@@ -0,0 +1,42 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+
+public abstract class ChessPiece extends Piece {
+
+	private Color color;
+	private Integer moveCount;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+		moveCount = 0;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public Integer getMoveCount() {
+		return moveCount;
+	}
+
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
+}
